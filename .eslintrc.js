2581586472cd0c35bced8cf344c3d369e258aeef
Chris Scott <cyberstrike@gmail.com>
2020-01-28 00:28:30 -0500

    added chai friendly linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: { node: true, browser: true, es6: true, jquery: true, mocha: true },
-  extends: ['plugin:vue/essential', '@vue/standard', 'plugin:cypress/recommended'],
+  extends: ['plugin:vue/essential', '@vue/standard', 'plugin:cypress/recommended', 'plugin:chai-friendly/recommended'],
   rules: {
     'no-console': 'off',
     'no-debugger': 'off'
